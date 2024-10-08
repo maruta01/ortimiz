cd59035c4be28d54b95dbd52eadba68a4698ab5d
jaipmarut <marutj@mbixtech.com>
2024-10-08 11:13:34 +0700

    init sign-in page

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,3 +1,44 @@
+'use client';
+
+import { Input } from "postcss";
+import Image from 'next/image';
+import styles from './styles.module.css';
+import { FormEvent, useRef, useState } from 'react';
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    const [loading, setLoading] = useState(false)
+
+
+    return (
+        <>
+            <div className="container-fluid">
+
+
+                <div className={`ort-signin-wrapper ${styles.background}`}>
+                    <div className="ort-card-signin">
+                        <span className={styles.form_logo}>
+                            <Image src='/images/logo/logo.png' alt='logo-login' height={280} width={280} />
+                        </span>
+                        <div className="ort-signin-header">
+                            <form action="index.html">
+                                <div className="form-group">
+                                    <label>Username</label>
+                                    <input type="text" className="form-control" placeholder="Enter your username" defaultValue="sunchate" />
+                                </div>
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input type="password" className="form-control" placeholder="Enter your password" defaultValue="" />
+                                </div>
+                                <button className="btn btn-ort-primary btn-block">Sign In</button>
+                            </form>
+                        </div>
+                        <div className="ort-signin-footer">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+
 }
---
+++ src/app/i18nConfig.ts
@@ -0,0 +1,7 @@
+const i18nConfig = {
+    locales: ['en', 'th'],
+    defaultLocale: 'en',
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
+};
+
+export default i18nConfig;
