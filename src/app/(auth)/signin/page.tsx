66bc2a29a47f65847aaf3511a30b4f062e4b51f0
jaipmarut <marutj@mbixtech.com>
2024-10-10 09:01:59 +0700

    update UI signIn

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
                 <div className={`ort-signin-wrapper ${styles.background}`}>
                     <div className="ort-card-signin">
                         <span className={styles.form_logo}>
-                            <Image src='/images/logo/logo.png' alt='logo-login' height={280} width={280} />
+                            <Image src='/images/logo/logo.png' alt='logo-login' height={250} width={250} />
                         </span>
                         <div className="ort-signin-header">
                             <form action="index.html">
@@ -29,7 +29,19 @@ export default function Page() {
                                     <label>Password</label>
                                     <input type="password" className="form-control" placeholder="Enter your password" defaultValue="" />
                                 </div>
-                                <button className="btn btn-ort-primary btn-block">Sign In</button>
+
+                                <div className="row ">
+                                    <div className="col">
+                                        <div className="form-check">
+                                            <input type="checkbox" className="form-check-input" id="remember_me_checkbox" style={{marginTop:'0.1rem'}}/>
+                                            <label className="form-check-label text-dark" htmlFor="remember_me_checkbox">Remember me</label>
+                                        </div>
+                                    </div>
+                                    <div className="col text-end">
+                                        <a href="#" className="text-dark">Forget paasword ? </a>
+                                    </div>
+                                </div>
+                                <button className="btn btn-ort-primary btn-block mt-4">Sign In</button>
                             </form>
                         </div>
                         <div className="ort-signin-footer">
