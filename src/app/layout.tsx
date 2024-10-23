42e19025511b235ceae843f0fcfde70ef7a5df52
jaipmarut <marutj@mbixtech.com>
2024-10-23 16:28:31 +0700

    import kanit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,29 +5,23 @@ import localFont from "next/font/local";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./ort.css"
 import "./globals.css";
-import Link from 'next/link'
 import React from "react";
 import Navbar from "../components/Navbar";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import '@fontsource/kanit'; // Defaults to weight 400
+import '@fontsource/kanit/300.css'; // Weight 400
+import '@fontsource/kanit/400.css'; // Weight 400
+import '@fontsource/kanit/500.css'; // Weight 400
+import '@fontsource/kanit/600.css'; // Weight 400
+import '@fontsource/kanit/700.css'; // Weight 700
 
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "ORTIMIZ",
   description: "KPI and Action Execution",
   icons: "/images/logo/Brand-Logo_0.svg"
 };
-
 
 
 export default function RootLayout({
@@ -38,9 +32,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-
-        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-
         <BootstrapClient />
       </head>
       <body>
